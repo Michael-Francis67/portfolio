2a1f426895f48cdc5d8ed18ee2d03e201d7a4efd
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-10-06 04:14:45 +0100

    deploying to verce and created vercel.json file and modified server.js file

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,14 @@ const __dirname = path.resolve();
 
 app.use(helmet({contentSecurityPolicy: false}));
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
-);
+if (process.env.NODE_ENV === "development") {
+    app.use(
+        cors({
+            origin: process.env.CLIENT_URL,
+            credentials: true,
+        })
+    );
+}
 
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
