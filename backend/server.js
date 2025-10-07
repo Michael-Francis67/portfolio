78e26128b4784a8002009e96b8fafbd5394b3ec5
Onuzulike Ikechukwu Michael <michealikechukwu06@gmail.com>
2025-10-07 12:14:05 +0100

    Last change in my portfolio

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,14 @@ import skillRoutes from "./routes/skills.js";
 import projectRoutes from "./routes/projects.js";
 import uploadRoutes from "./routes/upload.js";
 import CVRoutes from "./routes/cv.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(helmet({contentSecurityPolicy: false}));
 
@@ -31,12 +34,14 @@ app.get("/health", (req, res) => {
     res.status(200).send("OK");
 });
 
-app.use(
-    rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
-    })
-);
+if (process.env.NODE_ENV === "production") {
+    app.use(
+        rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100, // limit each IP to 100 requests per windowMs
+        })
+    );
+}
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({extended: true}));
@@ -53,6 +58,16 @@ app.use("/api/skills", skillRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/cv", CVRoutes);
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
